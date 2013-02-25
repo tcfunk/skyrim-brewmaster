d396c1e032e1a8b58c5d564dc6dc0649b4670f23
Tyler Funk <tcfunk25@gmail.com>
2013-02-24 20:48:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkyrimBrewmaster::Application.config.secret_token = '31dde96c4d7903353575b0323b0e4ef70f339913bc8d95613ab522f58dcd9623b360de0135b99a51e210b493646986ae74da7c0de64ed7f7c220dae9c85d8414'
